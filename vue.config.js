de6b6b4076b0bf2996067e0d82efd036d4ad7bfa
JohnWeeks1 <jonathanweeks007@gmail.com>
2022-11-13 20:59:13 +0700

    Added manifest to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,42 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // pwa: {
-  //   name: 'My App',
-  //   themeColor: '#1a1a1a',
-  //   msTileColor: '#1a1a1a',
-  //   appleMobileWebAppCapable: 'yes',
-  //   appleMobileWebAppStatusBarStyle: 'black',
-
-  //   // configure the workbox plugin
-  //   workboxPluginMode: 'InjectManifest',
-  //   workboxOptions: {
-  //     // swSrc is required in InjectManifest mode.
-  //     // swSrc: 'dev/sw.js',
-  //     // ...other Workbox options...
-  //   }
-  // }
+  pwa: {
+    manifest: {
+      name: 'Uber clone',
+      short_name: 'Uber clone',
+      description: 'This is an Uber clone',
+      display: 'standalone',
+      orientation: 'portrait',
+      background_color: '#1a1a1a',
+      theme_color: '#1a1a1a',
+      icons: [
+        {
+          src: 'icons/icon-128x128.png',
+          sizes: '128x128',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-256x256.png',
+          sizes: '256x256',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-384x384.png',
+          sizes: '384x384',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  },
 })
